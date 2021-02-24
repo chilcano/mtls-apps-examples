aab5382b52b442ef654a940a820fe3e8bd2ed9cd
Chilcano <chilcano@intix.info>
2021-02-24 10:24:25 +0100

    Lab 4 removed

--- 4-mtls-emojivoto-tf/modules/dns/data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_route53_zone" "playground_hostedzone" {
-  name         = var.domain_name
-  private_zone = false
-}
--- 4-mtls-emojivoto-tf/modules/instance/output.tf
+++
@@ -1,7 +0,0 @@
-output "public_ips" {
-  value       = aws_instance.main.*.public_ip
-  description = "The public ips of the workstation"
-}
-output "unique_identifiers" {
-  value = aws_instance.main.*.tags.Name
-}
